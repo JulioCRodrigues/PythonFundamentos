0966aef2bfef7bb07efdf9605d149a01d45b8005
Julio Rodrigues <julio_cesar.jcr@hotmail.com>
2020-07-30 21:10:49 -0300

    Variaveis e operadores

---
+++ VariaveisEOperadores.py
@@ -0,0 +1,30 @@
+# Variaveis e operadores
+# Exibir na tela
+var_teste = 50
+print(var_teste)
+
+var_texto = "Olá, mundo!"
+print(var_texto)
+
+#Exibe tipo da variavel = inteiro
+print(type(var_teste))
+
+#Exibe tipo da variavel = string
+print(type(var_texto))
+
+#Declaração multipla
+pessoa1, pessoa2, pessoa3 = "Maria", "José", "Paulo"
+print(pessoa1, pessoa2, pessoa3)
+
+#Usando variaveis para operações
+altura = 4
+largura = 2
+area = altura * largura
+print(area)
+
+#Concatenar variaveis
+nome = "Julio"
+sobrenome = "Rodrigues"
+
+fullname = nome  + " " + sobrenome
+print(fullname)
